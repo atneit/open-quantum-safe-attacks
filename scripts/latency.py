17db35bd669c62f01248d2a5f955cffdece80e45
Alexander Nilsson <a.code@nilsson.link>
2020-01-24 07:02:58 +0100

    Improved arguemnt ahndling in latency.py

--- scripts/latency.py
+++ scripts/latency.py
@@ -1,7 +1,3 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
 import sys
 
 if len(sys.argv) < 4:
@@ -12,8 +8,12 @@ csvname = sys.argv[1]
 percentage = float(sys.argv[2]) / 100
 columns = sys.argv[3:]
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-data = pd.read_csv('measurments.csv', sep=',', header=0)
+data = pd.read_csv(csvname, sep=',', header=0)
 for col in columns:
     d = data[col]
     last = int(len(d) * percentage)
