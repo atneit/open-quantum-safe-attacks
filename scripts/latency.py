9d31672a5e5e358715a5c0af77ee58c5570e15a9
Alexander Nilsson <a.code@nilsson.link>
2020-02-05 13:14:24 +0100

    Write graphs to pgf (latex) plots

--- scripts/latency.py
+++ scripts/latency.py
@@ -6,8 +6,7 @@ matplotlib.rcParams.update({
     "pgf.texsystem": "pdflatex",
     'font.family': 'serif',
     'text.usetex': True,
-    'pgf.rcfonts': False,
-    'font.size': 8,
+    'pgf.rcfonts': False
 })
 
 import matplotlib.pyplot as plt
@@ -15,18 +14,23 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 from pandas.core.algorithms import value_counts
+import re
+
+sns.set()
+sns.set_context("paper")
 
 
 if len(sys.argv) < 4:
-    print("Usage: data.csv output.pgf maximum-value COLUMN1 [COLUMN2 [COLUMN3 [...]]]")
+    print("Usage: data.csv output.pgf title maximum-value COLUMN1 [COLUMN2 [COLUMN3 [...]]]")
     sys.exit(1)
     
 csvname = sys.argv[1]
 pgfoutputname = sys.argv[2]
+title = sys.argv[3]
 #samplesize = int(sys.argv[2])
 percentage = 1.0#0.75  #float(sys.argv[3]) / 100
-maxval = int(sys.argv[3])
-colspec = sys.argv[4:]
+maxval = int(sys.argv[4])
+colspec = sys.argv[5:]
 
 
 data = pd.read_csv(csvname, sep=',', header=0)
@@ -34,8 +38,11 @@ data = pd.read_csv(csvname, sep=',', header=0)
 columns = []
 for spec in colspec:
     for col in data.columns:
-        if spec in col and col not in columns:
+        if col not in columns and re.search(spec, col):
             columns.append(col)
+
+if not columns:
+    sys.exit(0)
             
 print("Selected columns: " + repr(columns))
 
@@ -53,34 +60,36 @@ def getlabel(colname):
     elif "MAJOR" in colname:
         return "$x=2^{D-B}$"
     else:
-        return colname
+        return colname.replace("{", "\{").replace("}", "\}")
 
 
-colors = ["r", "g", "b"]
-
+colors = sns.color_palette("cubehelix", len(columns))
 axis = None
 height = 0.25
+heightstep = height / (len(columns)+1)
 prevmean = None
+prevmin = None
 for (i, col) in enumerate(columns):
-    height -= 0.05
+    height -= heightstep
     d = usecol(col)
     axis = sns.distplot(d, label=getlabel(col), kde=True, bins=40, ax=axis, kde_kws={'cut':0}, color=colors[i])
-    #axis = sns.kdeplot(d, label=col, cut=0, ax=axis, color=colors[i])
+    #axis = sns.kdeplot(d, label=getlabel(col), cut=0, ax=axis, color=colors[i])
     mean = d.mean()
+    minimum = d.min()
     if prevmean:
-        print("{} mean: {} (diff with previous: {})".format(col, mean, prevmean - mean))
-        prevmean = None
+        print("{} mean: {}, min: {} (diff with previous: {} and {})".format(col, mean, minimum, prevmean - mean, prevmin - minimum))
     else:
-        print("{} mean: {}".format(col, mean))
-        prevmean = mean
+        print("{} mean: {}, min: {}".format(col, mean, minimum))
+    prevmean = mean
+    prevmin = minimum
     axis = sns.rugplot([mean], height=height, ax=axis, color=colors[i])
 
 axis.autoscale()
 
 plt.xlabel("Reference clock-cycles")
-plt.ylabel("KDE")
-#plt.title("MEMCMP")
-plt.gcf().set_size_inches(w=5, h=1.75)
+plt.ylabel("Density")
+plt.title(title)
+plt.gcf().set_size_inches(w=5.1, h=1.5)
 plt.legend()
 plt.show()
 
