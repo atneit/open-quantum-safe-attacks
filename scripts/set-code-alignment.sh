8042e4fa3a80d4980602bee8de2cfd68ec8eb9b0
Alexander Nilsson <a.code@nilsson.link>
2020-03-06 08:56:24 +0100

    Updated the scripts

--- scripts/set-code-alignment.sh
+++ scripts/set-code-alignment.sh
@@ -9,5 +9,5 @@ export ALIGNMENT=$(( 1 + $1 % 1024 ))
 cat main/src/code_align.rs.tmpl | sed "s|//SHIFT_CODE|$( 
                                                while [[ $ALIGNMENT > 0 ]]; do
                                                    ALIGNMENT=$(( $ALIGNMENT - 1 ));
-                                                   echo -n 'let x=black_box(x+1);\n        ';
+                                                   echo -n 'let x = black_box(x + 1);\n        ';
                                                done )|g" >main/src/code_align.rs;
